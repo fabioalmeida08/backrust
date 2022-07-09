5aa1d471bb1b6b2000285f78356f503bdd422b48
Fábio <fabioalmeida0896@gmail.com>
2022-07-09 18:01:12 -0300

    feat: created a function to send email with the log of compression

---
+++ src/email.rs
@@ -0,0 +1,27 @@
+use dotenv::dotenv;
+use lettre::transport::smtp::authentication::Credentials;
+use lettre::{Message, SmtpTransport, Transport};
+use std::env;
+
+pub fn send_email(size: &String) {
+    dotenv().ok();
+
+    let email = Message::builder()
+        .from(env::var("EMAIL").unwrap().parse().unwrap())
+        .to(env::var("EMAIL").unwrap().parse().unwrap())
+        .subject("Backup Done")
+        .body(format!("Backup size: {}", size))
+        .unwrap();
+
+    let creds = Credentials::new(env::var("EMAIL").unwrap(), env::var("PASSWORD").unwrap());
+
+    let mailer = SmtpTransport::relay("smtp.gmail.com")
+        .unwrap()
+        .credentials(creds)
+        .build();
+
+    match mailer.send(&email) {
+        Ok(_) => println!("Email sent successfully!"),
+        Err(e) => panic!("Could not send email: {:?}", e),
+    }
+}
