9cd784f2523a1498626d075ac62e7cb756d12f3e
Fábio <fabioalmeida0896@gmail.com>
2022-10-15 15:54:37 -0300

    refactor: removed functions to another modules

--- src/compression.rs
+++ src/compression.rs
@@ -2,14 +2,9 @@ use crate::backup_paths::backup_folders;
 use chrono::{DateTime, Local};
 use flate2::write::GzEncoder;
 use flate2::Compression;
-use std::env;
-use std::fs::{self, File};
-use fs_extra::dir;
+use std::fs::File;
 
-pub fn compression() -> Result<String, Box<dyn std::error::Error>> {
-    let home = env::var("HOME")?;
-    let mut log = String::new();
-
+pub async fn compression() -> Result<String, Box<dyn std::error::Error>> {
     let filename: DateTime<Local> = Local::now();
     let filename: String = format!("{}.tar.gz", filename.format("%d_%m_%Y"));
 
@@ -22,22 +17,9 @@ pub fn compression() -> Result<String, Box<dyn std::error::Error>> {
     for path in paths {
         let folders = path.split("/").collect::<Vec<_>>();
         let folder_name = folders.last().unwrap();
-        
-        let path_size = dir::get_size(&path)?;
-        let path_size_in_mb = format!("{} with {:.2}MB \n", &path, path_size as f32 / 1048576.0);
-        
-        log.push_str(&path_size_in_mb);
-        
-        tar.append_dir_all(format!("backup/{}", folder_name), format!("{}", path))?;
+
+        tar.append_dir_all(format!("backup/{}", folder_name), path)?;
     }
 
-    fs::create_dir_all(format!("{}/backup", home))?;
-    fs::rename(&filename, format!("{}/backup/{}", home, filename))?;
-
-    let total_size = dir::get_size(format!("{}/backup/{}", home, filename))?;
-    let total_size = format!("Total Backup Size: {:.2} MB", (total_size as f64 / 1048576.0));
-
-    log.push_str(&total_size);
-
-    Ok(log)
+    Ok(filename)
 }
