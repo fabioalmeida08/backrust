5897ba8e4cc73853ef4b6d931314a9098347de8a
Fábio <fabioalmeida0896@gmail.com>
2022-10-15 15:53:06 -0300

    refactor: changed env var name

--- src/backup_paths.rs
+++ src/backup_paths.rs
@@ -5,13 +5,16 @@ use std::io::Read;
 
 pub fn backup_folders() -> Result<Vec<String>, std::io::Error> {
     dotenv().ok();
-    
-    let backup_file_paths = env::var("BACKUP_PATHS_FILE").unwrap();
-    let mut str = String::new();
-    let mut file = File::open(backup_file_paths).expect("Error in reading file");
-    file.read_to_string(&mut str).expect("Unable to read line");
-    
-    let paths: Vec<String> = str.split('\n').map(|x| x.to_string()).collect();
+
+    let backup_dir = env::var("DIRS_TO_BACKUP_FILE").unwrap();
+    let mut string = String::new();
+    let mut file = File::open(backup_dir).expect("Error when reading file");
+
+    file.read_to_string(&mut string)
+        .expect("unable to read line");
+
+    let mut paths: Vec<String> = string.split('\n').map(|x| x.to_string()).collect();
+    paths.pop();
 
     Ok(paths)
 }
